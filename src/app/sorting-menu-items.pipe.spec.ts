5c4855290456026b2de46c8272100f90c8dbaa0e
Julia <ip018@hdm-stuttgart.de>
2019-02-19 10:19:15 +0100

    Auto-generated commit

--- src/app/sorting-menu-items.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortingMenuItemsPipe } from './sorting-menu-items.pipe';
-
-describe('SortingMenuItemsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortingMenuItemsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
